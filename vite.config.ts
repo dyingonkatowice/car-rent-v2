42cd40ec58b961690d90d5c8fabbf7a6aee61cfa
Bartosz <bartoszchmielecki2003@gmail.com>
2025-01-22 13:50:07 +0100

    Local network hosting

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
- 
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  assetsInclude: ['**/*.jpg', '**/*.png', '**/*.jpeg', '**/*.gif', '**/*.svg'],
-})
+  assetsInclude: ["**/*.jpg", "**/*.png", "**/*.jpeg", "**/*.gif", "**/*.svg"],
+});
